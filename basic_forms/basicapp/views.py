816a00e280a320562c55bb2e877ee4b52a4250b0
Andrew Struk <andrewstruk@yahoo.co.uk>
2024-11-08 05:58:18 +0000

    added bootstrap and printed form content to console

--- basic_forms/basicapp/views.py
+++ basic_forms/basicapp/views.py
@@ -8,4 +8,16 @@ def index(request):
 
 def form_name_view(request):
     form = forms.FormName()
+
+    if request.method == 'POST':
+        form = forms.FormName(request.POST)
+
+        if form.is_valid():
+            print("VALIDATION SUCCESSS")
+            print("Name: "+form.cleaned_data['name'])
+            print("Email: "+form.cleaned_data['email'])
+            print("Text: "+form.cleaned_data['text'])
+
+
+
     return render(request, 'basicapp/form_page.html', {'form': form})
